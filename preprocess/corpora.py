e999bb4410771fe73271601142c3cae9aa6616a7
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2018-09-26 22:09:00 +0900

    feat: improved usability of dictionary class: added properties, methods

--- preprocess/corpora.py
+++ preprocess/corpora.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import os, sys, io
+from typing import Union
 from collections import Counter
 import warnings
 import pickle
@@ -46,6 +47,21 @@ class Dictionary(object):
         return len(self._id2token) - self._offset
 
     @property
+    def max_id(self) -> int:
+        return max(self._id2token.keys())
+
+    @property
+    def offset(self) -> int:
+        return self._offset
+
+    @property
+    def oov_id(self) -> Union[int, None]:
+        if self._oov:
+            return self._token2id[self.__oov]
+        else:
+            return None
+
+    @property
     def vocab(self):
         return self._token2id.keys()
 
@@ -78,5 +94,13 @@ class Dictionary(object):
     def transform(self, lst_token):
         return [self._token_to_id(token) for token in lst_token]
 
+    def iter_transform(self, iter_lst_token):
+        for lst_token in iter_lst_token:
+            yield self.transform(lst_token)
+
     def inverse_transform(self, lst_index):
         return [self._id_to_token(index) for index in lst_index]
+
+    def iter_inverse_transform(self, iter_lst_index):
+        for lst_index in iter_lst_index:
+            yield self.inverse_transform(lst_index)
